5bdd70785b28e2b895c4e0024c06c0732daee2e6
Lakshan Damayantha <damayanthalakshan8@gmail.com>
2025-05-24 20:04:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHADOW=y5ojiQyQ#YhWxfxZ_rqOoNVm37HqVNvBBHFCqKVQlXZ6LIEXxzEI",
+SESSION_ID: process.env.SESSION_ID || "SHONU-X-MD=y5ojiQyQ#YhWxfxZ_rqOoNVm37HqVNvBBHFCqKVQlXZ6LIEXxzEI",
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "true",
